15d86f6614e10b109098bec6c4827fd521927b8a
Lan Phan <lanc_phan@hotmail.com>
2017-12-13 17:03:49 -0500

    add optional:true to 'belongs_to :events' in images.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, optional: true
 
   scope :sixteen_by_nine, -> { where(ratio: "16_9").where("url like ?", '%RETINA%') }
   scope :four_by_three, -> { where(ratio: "4_3").where("url like ?", '%CUSTOM%') }
